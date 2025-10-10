7b01bd918ab9bec73335d07580fa8c14e42981a2
Kadu Ribeiro <kaduesr@gmail.com>
2025-10-09 23:00:16 -0300

    refactor: ajustar estilo de importação e estrutura de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
+    <return>
       <Header />
-    </>
+    </return>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
-function Header() { 
-    return (
-        <Header className={styles.header}>
-            <nav>
-                <h2>MyFlix</h2>
-                {/* Futuramente, aqui vou colocar uma barra de pesquisa */}
-            </nav>
-        </Header>
-    )
+function Header() {
+  return (
+    <Header className={styles.header}>
+      <nav>
+        <h2>MeuFlix</h2>
+      </nav>
+    </Header>
+  );
 }
 
 export default Header;
